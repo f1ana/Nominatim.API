476274010eb335ac2f367aeef6c061a1a219fe12
Christopher Flanagan <chris@flana.com>
2020-09-16 22:34:38 -0400

    Fix thread safety, update GeoJSON

--- src/Nominatim.API/Geocoders/ReverseGeocoder.cs
+++ src/Nominatim.API/Geocoders/ReverseGeocoder.cs
@@ -1,9 +1,7 @@
 
-using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Threading.Tasks;
-using System.Globalization;
 using Nominatim.API.Extensions;
 using Nominatim.API.Models;
 using Nominatim.API.Web;
--- src/Nominatim.API/Web/WebInterface.cs
+++ src/Nominatim.API/Web/WebInterface.cs
@@ -2,8 +2,9 @@
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Reflection;
+using System.Text;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.WebUtilities;
 using Newtonsoft.Json;
 using Nominatim.API.Contracts;
 
@@ -14,6 +15,11 @@ namespace Nominatim.API.Web {
     public static class WebInterface {
         private static readonly HttpClient _httpClient = new HttpClient();
 
+        static WebInterface() {
+            _httpClient.DefaultRequestHeaders.UserAgent.Clear();
+            _httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("f1ana.Nominatim.API", Assembly.GetExecutingAssembly().GetName().Version.ToString()));
+        }
+        
         /// <summary>
         ///     Send a request to the Nominatim server
         /// </summary>
@@ -22,15 +28,29 @@ namespace Nominatim.API.Web {
         /// <param name="parameters">Query string parameters</param>
         /// <returns>Deserialized instance of T</returns>
         public static async Task<T> GetRequest<T>(string url, Dictionary<string, string> parameters) {
-            var req = QueryHelpers.AddQueryString(url, parameters);
-
-            _httpClient.DefaultRequestHeaders.UserAgent.Clear();
-            _httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("f1ana.Nominatim.API", Assembly.GetExecutingAssembly().GetName().Version.ToString()));
+            var req = addQueryStringToUrl(url, parameters);
 
             var result = await _httpClient.GetStringAsync(req).ConfigureAwait(false);
             var settings = new JsonSerializerSettings {ContractResolver = new PrivateContractResolver()};
 
             return JsonConvert.DeserializeObject<T>(result, settings);
+        }
+
+        private static string addQueryStringToUrl(string url, IDictionary<string, string> parameters) {
+            if ((parameters?.Keys.Count ?? 0) == 0) {
+                return url;
+            }
+
+            var op = url.IndexOf('?') != -1;
+            var sb = new StringBuilder();
+            sb.Append(url);
+            foreach (var kvp in parameters) {
+                sb.Append(op ? '&' : '?');
+                sb.Append($"{UrlEncoder.Default.Encode(kvp.Key)}={UrlEncoder.Default.Encode(kvp.Value)}");
+                op = true;
+            }
+
+            return sb.ToString();
         }
     }
 }
